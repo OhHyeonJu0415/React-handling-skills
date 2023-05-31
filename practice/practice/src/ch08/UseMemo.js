4117cb2f7fc2332832c65d1a78dbebeb700d2449
OhHyeonJu0415 <guswn2209@gmail.com>
2023-05-31 14:17:19 +0900

    comment : ch08.4

--- practice/practice/src/ch08/UseMemo.js
+++ practice/practice/src/ch08/UseMemo.js
@@ -5,6 +5,7 @@ const getAverage = (numbers) => {
 
   if (numbers.length === 0) return 0;
 
+  //배열을 순서대로 순회하며 콜백함수의 실행 값을 누적하여 하나의 값으로 변환
   const sum = numbers.reduce((a, b) => a + b);
   return sum / numbers.length;
 };
@@ -17,12 +18,14 @@ const UseMemo = () => {
     setNumber(e.target.value);
   };
 
-  const onInsert = (e) => {
+  const onInsert = () => {
+    //버튼을 누르면 list 배열의 값이 바뀐다
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
   };
 
+  //list 배열의 값이 바뀌면 함수 실행, 바뀌지 않으면 이전 값 재사용
   const avg = useMemo(() => getAverage(list), [list]);
 
   return (
