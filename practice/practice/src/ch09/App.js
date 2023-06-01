547a3073f2eae1540e2751a000fc7c231bcdf67e
OhHyeonJu0415 <guswn2209@gmail.com>
2023-06-01 14:12:54 +0900

    ch09.2 : Sass 사용하기

---
+++ practice/practice/src/ch09/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SassComponent from "./SassComponent"; //ch09.2 Sass 사용하기
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
