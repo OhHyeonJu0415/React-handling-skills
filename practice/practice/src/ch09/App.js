4aed3d997f51a3706172ad732da2d020fb03bc51
OhHyeonJu0415 <guswn2209@gmail.com>
2023-06-05 13:51:32 +0900

    ch9.4.3 : 스타일에서 props 조회하기

--- practice/practice/src/ch09/App.js
+++ practice/practice/src/ch09/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import SassComponent from "./SassComponent"; //ch09.2 Sass 사용하기
 import CSSModule from "./CSSModule"; //ch09.3 CSS Module
+import StyledComponent from "./StyledComponent"; //ch09.4 styeld-component
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <SassComponent /> ch09.3 */}
-        <CSSModule />
+        {/* <SassComponent /> ch09.2 */}
+        {/* <CSSModule /> ch09.3 */}
+        <StyledComponent />
       </div>
     );
   }
