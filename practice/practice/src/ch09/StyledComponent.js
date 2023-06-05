b59b1a79f6eefa8e259e7b99c0c056cc8e04d885
OhHyeonJu0415 <guswn2209@gmail.com>
2023-06-05 18:06:21 +0900

    ch09.4 : styled-components

--- practice/practice/src/ch09/StyledComponent.js
+++ practice/practice/src/ch09/StyledComponent.js
@@ -1,11 +1,47 @@
 import React from "react";
 import styled, { css } from "styled-components";
+// 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우
+// css를 불러와야 한다.
+
+//styled-component 유틸 함수 사용하기
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+//위 size 객체에 다라 자동으로 media 쿼리 함수를 만들어준다
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+
+  return acc;
+}, {});
 
 const Box = styled.div`
   /* props 로 넣어준 값을 직접 전달해줄 수 있다. */
   background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  /* 기본적으로는 가로 크기 1024px에 가운데 정렬을 하고
+  가로 크기가 작아짐에 따라 크기를 줄이고
+  768px 미만이 되면 꽉 채운다 */
+
+  /* 미디어쿼리 사용하기 */
+  /* @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  } */
+
+  /* 유틸 함수 사용하기 */
+  ${media.desktop`width:768px`}
+  ${media.tablet`width:100%`}
 `;
 
 // & 문자를 사용하여 Sass처럼 자기 자신 선택 가능
