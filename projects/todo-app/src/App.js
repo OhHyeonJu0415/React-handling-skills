236a750ace1ae7ebf246c2a3a48ee523955a6abb
OhHyeonJu0415 <guswn2209@gmail.com>
2023-06-15 12:55:25 +0900

    3.2 항목 추가 기능 구현하기

--- projects/todo-app/src/App.js
+++ projects/todo-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1, //고유 id
+      text: "리액트의 기초 알아보기", //sodyd
+      checked: true, //완료 여부
+    },
+    {
+      id: 2,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "리액트의 기초 알아보기",
+      checked: false,
+    },
+  ]);
+
+  //ref를 사용하여 변수(고윳값으로 사용될 id) 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      //추가할 객체
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      //객체 추가하기 (불변성 유지하기)
+      setTodos(todos.concat(todo));
+
+      //nextId 1씩 추가하기
+      nextId.current += 1;
+    },
+    [todos]
   );
-}
+
+  return (
+    <TodoTemplate>
+      <TodoList todos={todos} />
+      <TodoInsert onInsert={onInsert} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
--- projects/todo-app/src/components/TodoInsert.js
+++ projects/todo-app/src/components/TodoInsert.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { MdAdd } from "react-icons/md";
 import "../styles/TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState("");
+
+  // 함수를 한번만 만들고 재사용하기 : useCallback
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue("");
+
+      //submit 이벤트의 새로고침 방지
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
